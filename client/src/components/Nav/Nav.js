94a4cea1aaee009a43c38aa37614c06ed1965103
Dennis <dennisalvey423@gmail.com>
2017-11-11 12:57:06 -0800

    added products to nav bar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -18,30 +18,10 @@ const Nav = () =>
         <ul class="nav navbar-nav navbar-right ml-auto">
           <li><a href="/"><span class="glyphicon glyphicon-home"></span> Home </a></li>
           <li><a href="/about"><span class="glyphicon glyphicon-info-sign"></span> About Us</a></li>
-          <li><a href="/login"><span class="glyphicon glyphicon-user"></span> Login/Register</a></li>
+          <li><a href="/Products">Products </a></li>
+          <li><a href="/login"><span class="glyphicon glyphicon-user"></span> Login/Register</a></li>          
         </ul>
       </div>
-<<<<<<< HEAD
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="/">Home </a></li>
-          <li><a href="/about">About Us</a></li>
-          <li><a href="/login">Login/Register</a></li>
-          <li><a href="/Products">Products </a></li>
-          {/* Dropdown Menu */}
-          {/* <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">AfterLogin - Dvlpmt Only
-            <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li class="divider"><a href="/customers">Customers</a></li>
-              <li class="divider"><a href="/dashboard">Dashboard</a></li>
-              <li class="divider"><a href="/schedule">Driving Routes/Schedule</a></li>
-            </ul>
-          </li> */}
-        {/* End of Dropdown Menu */}
-
-      </ul>
-=======
->>>>>>> master
     </div>
   </nav>
 
