099873da2771cedf4211a51738bed9ca165169d2
Zoey007 <guiyuinusa@gmail.com>
2017-11-13 14:27:03 -0800

    fixed images folder, fixed About Us layout and image, added image to Contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -9,20 +9,45 @@ import { Link } from "react-router-dom";
 
 const Contact = () =>
 
+<div>
+
 <Container>
-  {/* Row #1 */}
   <Row>
-    <Col size="md-12">
-      <Jumbotron>
-        <h1><strong>Contact Us</strong></h1>
-      </Jumbotron>
-
-      {/* Zotabox Contact Form Embed */}
-      <div style={{margin:'0 auto', padding: "20px"}} id="zbwid-e422a183"></div>  
-
+    <Col size="md-12">   
+      <div className="banner container" 
+        /* Crop the banner image */
+        style={{width: '100%', height: '250px', overflow: 'hidden'}}>   
+        <img className="banner img-responsive" src="/images/call-center-istockphoto-178570372.jpg" 
+        /* Pan banner inside banner container */
+        style={{position:'relative', 'margin': '-80px 0 80px 0'}}/> 
+        <div className="carousel-caption" style={{'text-shadow': '2px 2px 4px #000000'}}>
+          <h1><strong>Contact Us</strong></h1>
+        </div>
+      </div>
     </Col>
   </Row>
 
 </Container>
 
+<Container>
+  {/* Row #2 */}
+  {/* <Row>
+    <Col size="md-12">
+      <Jumbotron>
+        <h1><strong>Contact Us</strong></h1>
+      </Jumbotron>
+    </Col>
+  </Row> */}
+
+  {/* Row #3 */}
+  {/* Zotabox Contact Form Embed */}
+  <Row>
+    <Col size="md-12">
+      <center><div style={{padding: "20px 0 20px 0"}} id="zbwid-e422a183"></div></center>  
+
+    </Col>
+  </Row>
+</Container>
+
+</div>
 export default Contact;
