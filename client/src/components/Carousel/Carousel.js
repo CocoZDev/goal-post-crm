dbd3c63e4c91cc5f0d86f6aa6c60bd11454a507f
Zoey007 <guiyuinusa@gmail.com>
2017-11-15 15:18:47 -0800

    fixed carousel overlay

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -3,9 +3,8 @@ import "../../styles/Carousel.css";
 
 const Carousel = () =>
 <div className="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
-    {/* <!-- Overlay --> */}
-    <div className="overlay"></div>
 
+{/* Bootstrap carousel with overlay */}
     {/* <!-- Indicators --> */}
     <ol className="carousel-indicators">
     <li data-target="#bs-carousel" data-slide-to="0" className="active"></li>
@@ -16,33 +15,39 @@ const Carousel = () =>
     {/* <!-- Wrapper for slides --> */}
     <div className="carousel-inner" style={{color:'white'}}>
     <div className="item slides active">
-        <div className="slide-1"></div>
-        <div className="carousel-caption">
-        <hgroup>
-            <h1>We are convenient</h1>        
-            <h3>Access all useful sales tools in one place.</h3>
-        </hgroup>
-        <button className="btn btn-hero btn-lg" role="button" style={{color:'white'}}><a href="/login">Join Us Today</a></button>
+        <div className="slide-1">
+            <div className="overlay"></div> 
+        </div>
+        <div className="hero">
+            <hgroup>
+                <h1>We are convenient</h1>        
+                <h3>Access all useful sales tools in one place.</h3>
+            </hgroup>
+            <button className="btn btn-hero btn-lg" role="button"><a href="/login">Join Us Today</a></button>
         </div>
     </div>
     <div className="item slides">
-        <div className="slide-2"></div>
-        <div className="carousel-caption">        
-        <hgroup>
-            <h1>We are efficient</h1>        
-            <h3>Boost your sales team's productivity</h3>
-        </hgroup>       
-        <button className="btn btn-hero btn-lg" role="button">Join Us Today</button>
+        <div className="slide-2">
+            <div className="overlay"></div>    
+        </div>
+        <div className="hero">        
+            <hgroup>
+                <h1>We are efficient</h1>        
+                <h3>Boost your sales team's productivity</h3>
+            </hgroup>       
+            <button className="btn btn-hero btn-lg" role="button">Join Us Today</button>
         </div>
     </div>
     <div className="item slides">
-        <div className="slide-3"></div>
-        <div className="carousel-caption">        
-        <hgroup>
-            <h1>We are mobile</h1>        
-            <h3>Access your data from any device with Internet connection</h3>
-        </hgroup>
-        <button className="btn btn-hero btn-lg" role="button">Join Us Today</button>
+        <div className="slide-3">
+           <div className="overlay"></div>    
+        </div>
+        <div className="hero">        
+            <hgroup>
+                <h1>We are mobile</h1>        
+                <h3>Access your data from any device with Internet connection</h3>
+            </hgroup>
+            <button className="btn btn-hero btn-lg" role="button">Join Us Today</button>
         </div>
     </div>
     </div> 
