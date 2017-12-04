b19a0a4f9ff43ad0dc3f2e7ba6c09a6dd68e8360
Guiyu Zhao <guiyuinusa@gmail.com>
2017-12-03 16:37:08 -0800

    changed the Login Form style

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -2,5 +2,5 @@ import React from "react";
 
 export const Input = props =>
   <div className="form-group">
-    <input className="form-control input-large" style={{ textAlign: "center"}}{...props} />
+    <center><input className="form-control input-large" style={{ textAlign: "center"}}{...props} /></center>
   </div>;
