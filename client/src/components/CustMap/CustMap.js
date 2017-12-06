a6b446b2b04af1f952c794008e2cb76b9c5927d7
jeanineerica <jeanineerica@me.com>
2017-12-06 13:20:29 -0800

    working on map

--- client/src/components/CustMap/CustMap.js
+++ client/src/components/CustMap/CustMap.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+// import React, { Component } from "react";
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {lat: 59.95, lng: 30.33},
-    zoom: 11
-  };
-
-  render() {
-    return (
-      <GoogleMapReact
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Kreyser Avrora'}
-        />
-      </GoogleMapReact>
-    );
-  }
-}
+// export class MapContainer extends Component {
+//   render() {
+//     const style = {
+//       width: '100%',
+//       height: '100%'
+//     }
+//       return (
+//         <Map google={this.props.google} zoom={14}>
+  
+//           <Marker onClick={this.onMarkerClick}
+//                   name={'Current location'} />
+  
+//           {/* <InfoWindow onClose={this.onInfoWindowClose}>
+//               <div>
+//                 <h1>{this.state.selectedPlace.name}</h1>
+//               </div>
+//           </InfoWindow> */}
+//         </Map>
+//       );
+//     }
+//   }
+  
+//   export default GoogleApiWrapper({
+//     apiKey: "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo"
+//   })(MapContainer)
